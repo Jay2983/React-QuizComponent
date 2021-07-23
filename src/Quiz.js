9a25ba7c0a05326597343de08c0c35dc9e1cc4ed
Vamshree Siramdasu <vamshree@outlook.com>
2021-07-23 23:40:15 +1000

    Module 8 - Take 1

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,12 +15,16 @@ class Quiz extends Component{
         })
 
     }
+    handleResetQuiz(){
+
+    }
+
     render(){
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
 
         return(
             <div>
-                {isQuizEnd ? <QuizEnd/> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>}
+                {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetQuiz.bind(this)}/> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>}
             </div>
         )        
     };
